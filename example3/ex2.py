fa5c034f04dc65fbec2614341626e3aeccf1b81e
Erick Ramirez <erick@bitso.com>
2025-02-13 21:49:45 -0500

    2 examples

---
+++ example3/ex2.py
@@ -0,0 +1,41 @@
+import json
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from pydantic import BaseModel, Field
+import getpass
+import os
+
+if not os.environ.get("OPENAI_API_KEY"):
+  os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+class Classification(BaseModel):
+    sentiment: str = Field(..., enum=["happy", "neutral", "sad"])
+    aggressiveness: int = Field(
+        ...,
+        description="describes how aggressive the statement is, the higher the number the more aggressive",
+        enum=[1, 2, 3, 4, 5],
+    )
+    language: str = Field(
+        ..., enum=["spanish", "english", "french", "german", "italian"]
+    )
+
+tagging_prompt = ChatPromptTemplate.from_template(
+    """
+Extract the desired information from the following passage.
+
+Only extract the properties mentioned in the 'Classification' function.
+
+Passage:
+{input}
+"""
+)
+
+
+llm = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0.1).with_structured_output(Classification)
+
+inp = "Estoy increiblemente contento de haberte conocido! Creo que seremos muy buenos amigos!"
+prompt = tagging_prompt.invoke({"input": inp})
+print(llm.invoke(prompt))
